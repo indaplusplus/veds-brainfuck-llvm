ff15388879fd0fff96a4501962d3d12d617d2daa
Viktor Edström <veds@kth.se>
2017-11-09 12:47:51 +0100

    small changes

--- brainfuck.c
+++ brainfuck.c
@@ -1,10 +1,13 @@
+/*
+ * Uses llvm 4.0 (currently stable) -
+ * https://releases.llvm.org/4.0.0/docs/index.html
+ */
 #include <stdio.h>
 #include <stdlib.h>
-//llvm 4.0 (currently stable) - https://releases.llvm.org/4.0.0/docs/index.html
 #include <llvm-c/Core.h>
 #include <llvm-c/BitWriter.h>
 
-#define BF_STACK_SIZE 100000
+#define BF_MEMORY_SIZE 100000
 
 typedef struct {
     /**
@@ -13,41 +16,43 @@ typedef struct {
     LLVMValueRef calloc_func;   //calloc since we need to zero-initialize the array.
     LLVMValueRef putchar_func;
     LLVMValueRef getchar_func;
-
     LLVMBuilderRef builder;
     LLVMValueRef main;
-
     LLVMValueRef bf_data;
     LLVMValueRef bf_ptr;
-}brainfuck;
+}Brainfuck;
 
 typedef struct {
     LLVMBasicBlockRef condition;
     LLVMBasicBlockRef body;
     LLVMBasicBlockRef end;
-}brainfuck_loop;
+}Brainfuck_loop;
 
 char *read_bf_file(char *filename) {
     FILE *f = fopen(filename, "r");
-    
+
     if (f == NULL) {
         abort();
     }
 
-    fseek(f, 0L, SEEK_END); //move to the end
+    fseek(f, 0L, SEEK_END);
     size_t file_size = ftell(f);
     rewind(f);
 
-    char *buffer = malloc(sizeof(char) * file_size + 1);
+    char *buffer;
+    if ((buffer = malloc(sizeof(char) * file_size + 1)) == NULL) {
+        abort();
+    }
     fread(buffer, sizeof(char), file_size, f);
-    
-    buffer[file_size + 1] = '\0'; //string terminate buffer
+
+    //string terminate buffer
+    buffer[file_size + 1] = '\0';
     fclose(f);
 
     return buffer;
 }
 
-void llvm_brainfuck_inc_data(brainfuck *bf, int val) {
+void llvm_brainfuck_inc_data(Brainfuck *bf, int val) {
     LLVMValueRef one = LLVMConstInt(LLVMInt32Type(), val, 0);
 
     LLVMValueRef tmp = LLVMBuildLoad(bf->builder, bf->bf_ptr, "tmp_inc_data");
@@ -63,11 +68,11 @@ void llvm_brainfuck_inc_data(brainfuck *bf, int val) {
     );
 }
 
-void llvm_brainfuck_dec_data(brainfuck *bf, int val) {
+void llvm_brainfuck_dec_data(Brainfuck *bf, int val) {
     llvm_brainfuck_inc_data(bf, -val);
 }
 
-void llvm_brainfuck_inc_ptr(brainfuck *bf, int val) {
+void llvm_brainfuck_inc_ptr(Brainfuck *bf, int val) {
     LLVMValueRef one = LLVMConstInt(LLVMInt32Type(), val, 0);
     LLVMValueRef indices[1] = { one };
 
@@ -84,11 +89,11 @@ void llvm_brainfuck_inc_ptr(brainfuck *bf, int val) {
     );
 }
 
-void llvm_brainfuck_dec_ptr(brainfuck *bf, int val) {
+void llvm_brainfuck_dec_ptr(Brainfuck *bf, int val) {
     llvm_brainfuck_inc_ptr(bf, -val);
 }
 
-void llvm_brainfuck_putchar(brainfuck *bf) {
+void llvm_brainfuck_putchar(Brainfuck *bf) {
     LLVMValueRef tmp = LLVMBuildLoad(bf->builder, bf->bf_ptr, "tmp_putchar");
 
     LLVMValueRef putchar_args[] = {
@@ -109,7 +114,7 @@ void llvm_brainfuck_putchar(brainfuck *bf) {
     );
 }
 
-void llvm_brainfuck_getchar(brainfuck *bf) {
+void llvm_brainfuck_getchar(Brainfuck *bf) {
     LLVMBuildStore(
         bf->builder,
         LLVMBuildTrunc(
@@ -128,16 +133,16 @@ void llvm_brainfuck_getchar(brainfuck *bf) {
     );
 }
 
-void llvm_brainfuck_start_loop(brainfuck *bf, brainfuck_loop *loop) {
+void llvm_brainfuck_start_loop(Brainfuck *bf, Brainfuck_loop *loop) {
     loop->condition = LLVMAppendBasicBlock(bf->main, "condition");
     loop->body = LLVMAppendBasicBlock(bf->main, "body");
     loop->end = LLVMAppendBasicBlock(bf->main, "end");
 
     LLVMBuildBr(bf->builder, loop->condition);
     LLVMPositionBuilderAtEnd(bf->builder, loop->condition);
-    
+
     LLVMValueRef tmp = LLVMBuildLoad(bf->builder, bf->bf_ptr, "tmp_start_loop");
-    
+
     LLVMIntPredicate cmpne = LLVMIntNE;
     LLVMBuildCondBr(
         bf->builder,
@@ -158,34 +163,34 @@ void llvm_brainfuck_start_loop(brainfuck *bf, brainfuck_loop *loop) {
     LLVMPositionBuilderAtEnd(bf->builder, loop->body);
 }
 
-void llvm_brainfuck_end_loop(brainfuck *bf, brainfuck_loop *loop) {
+void llvm_brainfuck_end_loop(Brainfuck *bf, Brainfuck_loop *loop) {
     LLVMBuildBr(bf->builder, loop->condition);
     LLVMPositionBuilderAtEnd(bf->builder, loop->end);
 }
 
-void interprete_brainfuck(char *bf_program, brainfuck *bf) {
-    brainfuck_loop loops[1000];
-    brainfuck_loop *loop_ptr = loops;
-    
+void interprete_brainfuck(char *bf_program, Brainfuck *bf) {
+    Brainfuck_loop loops[1000];
+    Brainfuck_loop *loop_ptr = loops;
+
     char c;
     while((c = *bf_program) != '\0') {
         switch (c) {
             case '+':
-                llvm_brainfuck_inc_data(bf, 1);          
+                llvm_brainfuck_inc_data(bf, 1);
             break;
 
             case '-':
                 llvm_brainfuck_dec_data(bf, 1);
             break;
-           
+
             case '>':
                 llvm_brainfuck_inc_ptr(bf, 1);
             break;
-            
+
             case '<':
                 llvm_brainfuck_dec_ptr(bf, 1);
             break;
-            
+
             case '.':
                 llvm_brainfuck_putchar(bf);
             break;
@@ -197,7 +202,7 @@ void interprete_brainfuck(char *bf_program, brainfuck *bf) {
             case '[':
                 loop_ptr++;
                 llvm_brainfuck_start_loop(bf, loop_ptr);
-                
+
             break;
 
             case ']':
@@ -214,41 +219,45 @@ void llvm_brainfuck(char *bf_filename) {
     LLVMModuleRef module = LLVMModuleCreateWithName("bf_llvm");
     LLVMTypeRef main_ret = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
     LLVMValueRef main_value_ref = LLVMAddFunction(module, "main", main_ret);
-    
+
     LLVMBasicBlockRef main = LLVMAppendBasicBlock(main_value_ref, "bf_main");
     LLVMBuilderRef builder = LLVMCreateBuilder();
     LLVMPositionBuilderAtEnd(builder, main);
-    
-    brainfuck *bf = malloc(sizeof(brainfuck));
+
+    Brainfuck *bf;
+    if ((bf = malloc(sizeof(Brainfuck))) == NULL) {
+        abort();
+    }
+
     bf->builder = builder;
     bf->main = main_value_ref;
 
     LLVMValueRef one = LLVMConstInt(LLVMInt32Type(), 1, 0);
-    
+
     /*
      * setup calloc
      */
     LLVMTypeRef calloc_types[] = { LLVMInt32Type(), LLVMInt32Type() };
     LLVMTypeRef calloc_type = LLVMFunctionType(
-        LLVMPointerType(LLVMInt32Type(), 0), 
-        calloc_types, 
-        0, 
+        LLVMPointerType(LLVMInt32Type(), 0),
+        calloc_types,
+        0,
         1
-    ); 
+    );
     LLVMValueRef calloc = LLVMAddFunction(module, "calloc", calloc_type);
-    
+
     bf->calloc_func = calloc;
-    
+
     /*
      * setup putchar
      */
     LLVMTypeRef putchar_types[] = {LLVMInt32Type()};
     LLVMTypeRef putchar_type = LLVMFunctionType(
-        LLVMPointerType(LLVMInt32Type(), 0), 
-        putchar_types, 
-        0, 
+        LLVMPointerType(LLVMInt32Type(), 0),
+        putchar_types,
+        0,
         1
-    ); 
+    );
     LLVMValueRef putchar = LLVMAddFunction(module, "putchar", putchar_type);
     bf->putchar_func = putchar;
 
@@ -257,23 +266,23 @@ void llvm_brainfuck(char *bf_filename) {
      */
     LLVMTypeRef getchar_types[] = {LLVMInt32Type()};
     LLVMTypeRef getchar_type = LLVMFunctionType(
-        LLVMPointerType(LLVMInt32Type(), 0), 
-        getchar_types, 
-        0, 
+        LLVMPointerType(LLVMInt32Type(), 0),
+        getchar_types,
+        0,
         0
-    ); 
+    );
     LLVMValueRef getchar = LLVMAddFunction(module, "getchar", getchar_type);
     bf->getchar_func = getchar;
 
 
     /*
-     * allocate the brainfuck stack
+     * allocate memory for brainfuck
      */
     LLVMValueRef calloc_args[2] = {
-        LLVMConstInt(LLVMInt32Type(), BF_STACK_SIZE, 0),
+        LLVMConstInt(LLVMInt32Type(), BF_MEMORY_SIZE, 0),
         LLVMConstInt(LLVMInt32Type(), 1, 0)
-    }; 
-    
+    };
+
     LLVMValueRef data_ptr = LLVMBuildCall(builder, calloc, calloc_args, 2, "calloc_data_ptr");
     bf->bf_data = LLVMBuildAlloca(builder, LLVMPointerType(LLVMInt32Type(), 0), "data");
     bf->bf_ptr = LLVMBuildAlloca(builder, LLVMPointerType(LLVMInt32Type(), 0), "ptr");
@@ -281,11 +290,13 @@ void llvm_brainfuck(char *bf_filename) {
     LLVMBuildStore(builder, data_ptr, bf->bf_data);
     LLVMBuildStore(builder, data_ptr, bf->bf_ptr);
 
-    interprete_brainfuck(read_bf_file(bf_filename), bf);
+    char *bf_file_content = read_bf_file(bf_filename);
+    interprete_brainfuck(bf_file_content, bf);
 
     LLVMBuildRet(builder, one);
     LLVMWriteBitcodeToFile(module, "brainfuck.bc");
     LLVMDisposeBuilder(builder);
+    free(bf_file_content);
 }
 
 int main(int argc, char *argv[]) {
